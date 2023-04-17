40bcb389d43e84cdb4667ce9ef2521701bc0478c
Thisistheway16 <129109918+Thisistheway16@users.noreply.github.com>
2023-04-17 15:27:18 -0400

    Update Code Skeleton.py

---
+++ Code Skeleton.py
@@ -0,0 +1,14 @@
+def printMenu():
+    """
+This function prints the menu options for the customer and sales system.
+    """
+    print('''
+          Customer and Sales System\n
+          1. Enter Customer Information\n
+          2. Generate Customer data file\n
+          3. Report on total Sales (Not done in this part)\n
+          4. Check for fraud in sales data (Not done in this part)\n
+          9. Quit\n
+          Enter menu option (1-9)
+          ''')
+    
