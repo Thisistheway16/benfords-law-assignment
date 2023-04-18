60dfafeb6a58343dde73d325750b3a5f388ed32f
Thisistheway16 <129109918+Thisistheway16@users.noreply.github.com>
2023-04-18 19:44:21 -0400

    Loads the sales Data complete

---
+++ Salesanalasis.py
@@ -0,0 +1,6 @@
+import csv
+
+with open("sales (1).csv", 'r') as file: #opens the csv file
+  csvreader = csv.reader(file)# Reads the csv File
+  for row in csvreader:
+    print(row[1], end = '\t')#prints the second collumn and prints it in rows
