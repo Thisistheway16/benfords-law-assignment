0fea87fc298e2a9cafb09122a035832bb4fc38ab
leon <reelstormmy@gmail.com>
2023-04-17 22:40:17 -0400

    Added base for checking for fraud (banfords law)

--- Code Skeleton.py
+++ Code Skeleton.py
@@ -13,8 +13,28 @@ This function prints the menu options for the customer and sales system.
           ''')
     
 
-def AnalyzeData():
+def analyzeData():
     print("Analyzing data")
 
 
 def checkForBenfordsLaw():
+    numbers = { }
+    amountOfLines = 0
+
+    # Open the sales.csv file and read each line, line by line
+    with open("sales.csv") as openedFile:
+        for line in openedFile:
+
+            # Get the digit to add to the dictionary
+            numberToAdd = line.split(',')[1][0] # [1] represents the sales column, [0] represents the first letter of the number in the sales column 
+
+            if numberToAdd in numbers:
+                numbers[numberToAdd] += 1
+            else:
+                numbers[numberToAdd] = 1
+
+        
+
+
+
+checkForBenfordsLaw()
