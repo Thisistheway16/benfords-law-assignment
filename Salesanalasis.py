7115783ed6057ae94018ad9b39e601c49d509e0f
Thisistheway16 <129109918+Thisistheway16@users.noreply.github.com>
2023-04-19 10:35:12 -0400

    Loads all the numbers in the sales.csv file and lists them in the terminal

--- Salesanalasis.py
+++ Salesanalasis.py
@@ -1,6 +1,7 @@
-import csv
+def salesAnalasis():
+    import csv
 
-with open("sales (1).csv", 'r') as file: #opens the csv file
-  csvreader = csv.reader(file)# Reads the csv File
-  for row in csvreader:
-    print(row[1], end = '\t')#prints the second collumn and prints it in rows
+    with open("sales (1).csv", 'r') as file: #opens the csv file
+     csvreader = csv.reader(file)# Reads the csv File
+    for row in csvreader:
+        print(row[1], end = '\t')#prints the second collumn and prints it in rows
