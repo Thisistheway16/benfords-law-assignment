37cc0dbfe6f15da553e6b29503baf07628a28333
Thisistheway16 <129109918+Thisistheway16@users.noreply.github.com>
2023-04-19 12:33:41 -0400

    Loads the Data and reads sales (1).csv

--- Salesanalasis.py
+++ Salesanalasis.py
@@ -1,7 +1,24 @@
-def salesAnalasis():
-    import csv
+import csv
+import os
 
-    with open("sales (1).csv", 'r') as file: #opens the csv file
-     csvreader = csv.reader(file)# Reads the csv File
-    for row in csvreader:
-        print(row[1], end = '\t')#prints the second collumn and prints it in rows
+def loadData():
+    while True:
+        filePath = input("Enter the file path for sales data (sales (1).csv): ")
+        if not os.path.isfile(filePath):
+            print("Error: invalit file path. Please try again: ")
+            continue
+        if not filePath.endswith(".csv"):
+            print("Error: invalid file extension. Please try again: ")
+            continue
+
+        salesData = []
+
+        with open(filePath, 'r') as file:
+            csvReader = csv.reader(file, delimiter = ",")
+            for row in csvReader:
+                salesData.append(row[1])
+        print(salesData)
+        return salesData
+    
+    
+    
